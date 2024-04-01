fb6aef3c33e57856427e3ee783267b3a4d0845c2
Russell Dunphy <russell@russelldunphy.com>
2024-04-01 17:24:04 +0100

    Remove DisallowExtraKeys

--- src/schema/createModel.ts
+++ src/schema/createModel.ts
@@ -1,5 +1,6 @@
+import { ColumnDefinition } from "../types/schema/definition/ColumnDefinition";
 import { ModelDefinition } from "../types/schema/definition/ModelDefinition";
 
-export const createModel = <Model extends ModelDefinition>(
-    model: Model,
-): Model => model;
+export const createModel = <Columns extends Record<string, ColumnDefinition>>(
+    model: ModelDefinition<Columns>,
+): ModelDefinition<Columns> => model;
