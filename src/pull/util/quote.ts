1f5bfdef745395b1de76062f4765d97b067cea95
Russell Dunphy <russell@russelldunphy.com>
2024-04-02 22:51:48 +0100

    Fix quoting of reserved word column and table names in generated model definitions

--- src/pull/util/quote.ts
+++ src/pull/util/quote.ts
@@ -1,1 +1,11 @@
-export const quote = (s: string) => JSON.stringify(s);
+export const quote = (s: string) => {
+    // if we're given an already quoted string,
+    // strip the quotes and re-quote it.
+    // this is needed because postgres returns column or table
+    // names that use reserved words in quotes.
+    if (s.startsWith('"') && s.endsWith('"')) {
+        return JSON.stringify(s.substring(1, s.length - 1));
+    } else {
+        return JSON.stringify(s);
+    }
+};
