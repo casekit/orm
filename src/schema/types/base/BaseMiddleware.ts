ef93975370a25355c0610725ed0311c2c812e6b9
Russell Dunphy <russell@russelldunphy.com>
2024-04-13 08:57:38 +0100

    Fix prettier error

--- src/schema/types/base/BaseMiddleware.ts
+++ src/schema/types/base/BaseMiddleware.ts
@@ -2,20 +2,17 @@ import { WhereClause } from "../../../queries/types/WhereClause";
 import { ModelDefinitions } from "../definitions/ModelDefinitions";
 import { BaseConfiguration } from "./BaseConfiguration";
 
-export type BaseWhereMiddleware<
-> = (
+export type BaseWhereMiddleware = (
     config: BaseConfiguration,
     m: string,
     where?: WhereClause<ModelDefinitions, string>,
 ) => WhereClause<ModelDefinitions, string> | undefined;
 
-export type BaseValuesMiddleware<
-> = (
+export type BaseValuesMiddleware = (
     config: BaseConfiguration,
     m: string,
     values?: Record<string, unknown | null>,
 ) => Record<string, unknown | null>;
-
 
 export type BaseMiddleware = {
     find: {
