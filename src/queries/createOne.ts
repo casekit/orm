27ef4eabab71ed40a7bbfbab4eb4ce0bea4ee623
Russell Dunphy <russell@russelldunphy.com>
2024-04-21 13:59:32 +0100

    Throw error if invalid combination of onConflict + returning is used

--- src/queries/createOne.ts
+++ src/queries/createOne.ts
@@ -1,5 +1,6 @@
 import { BaseConfiguration } from "src/schema/types/base/BaseConfiguration";
 
+import { OrmError } from "..";
 import { Connection } from "../Connection";
 import { BaseCreateOneParams } from "./create/types/BaseCreateOneParams";
 import { createMany } from "./createMany";
@@ -10,6 +11,14 @@ export const createOne = async (
     m: string,
     params: BaseCreateOneParams,
 ) => {
+    if (params.onConflict?.do === "nothing" && params.returning) {
+        throw new OrmError(
+            "Cannot use 'returning' with 'onConflict' in createOne",
+            {
+                data: { m, params },
+            },
+        );
+    }
     const result = await createMany(conn, config, m, {
         ...params,
         values: [params.values],
